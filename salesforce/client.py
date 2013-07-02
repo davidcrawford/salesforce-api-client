90a9e5c5618e4ed8cfbceac1fa5d05d6c6f0ce2f
David Crawford <david.crawford@gmail.com>
2013-07-01 22:49:31 -0700

    Take credentials in init

--- salesforce/client.py
+++ salesforce/client.py
@@ -7,15 +7,16 @@ class Client(object):
     '''Wraps an httplib2.Http client and constructs the URI at request time
     using the instance_url returned by the Salesforce API token request.'''
 
-    def __init__(self, http):
+    def __init__(self, http, credentials):
         self.http = http
+        self.credentials = credentials
 
     def request(self, uri, method='GET', body=None, headers=None,
                 redirections=httplib2.DEFAULT_MAX_REDIRECTS,
                 connection_type=None):
         instance_url = DEFAULT_INSTANCE_URL
-        if hasattr(self.request, 'credentials'):
-            token_response = self.request.credentials.token_response
+        if self.credentials:
+            token_response = self.credentials.token_response
             if token_response:
                 instance_url = token_response.get('instance_url',
                                                   DEFAULT_INSTANCE_URL)
@@ -25,9 +26,9 @@ class Client(object):
         except httplib2.MalformedHeader as e:
             if e.message.lower() != 'www-authenticate':
                 raise
-            self.request.credentials.refresh(self.http)
-            self.request.credentials.apply(headers)
-            instance_url = self.request.credentials.token_response.get(
+            self.credentials.refresh(self.http)
+            self.credentials.apply(headers)
+            instance_url = self.credentials.token_response.get(
                 'instance_url')
             return self.http.request(instance_url + uri, method, body,
                                      headers, redirections, connection_type)
